e05b0350c2de94675e5ba563e22512a13565aabd
JeK2a <vipjonpc@mail.ru>
2018-07-03 21:46:07 +0300

    Доработана структура.

--- src/java/PopupAuthenticator.java
+++ src/java/PopupAuthenticator.java
@@ -9,11 +9,16 @@ public class PopupAuthenticator extends javax.mail.Authenticator {
         String username;
         String password;
 
-        String result = JOptionPane.showInputDialog("Enter 'username,password'");
+//        String result = JOptionPane.showInputDialog("Enter 'username,password'");
+//
+//        StringTokenizer st = new StringTokenizer(result, ",");
+//        username = st.nextToken();
+//        password = st.nextToken();
 
-        StringTokenizer st = new StringTokenizer(result, ",");
-        username = st.nextToken();
-        password = st.nextToken();
+//        username = "vipjonpc@mail.ru";
+//        password = "webmailnokia35101989";
+        username = "jek2ka2016@yandex.ru";
+        password = "Nokia3510!";
 
         return new PasswordAuthentication(username, password);
     }
