c40dc521a9355f83c4f8ba94c4797baf3c0fe622
JeK2a <31507511+JeK2a@users.noreply.github.com>
2018-07-04 00:40:25 +0300

    Update PopupAuthenticator.java

--- src/java/PopupAuthenticator.java
+++ src/java/PopupAuthenticator.java
@@ -9,16 +9,11 @@ public class PopupAuthenticator extends javax.mail.Authenticator {
         String username;
         String password;
 
-//        String result = JOptionPane.showInputDialog("Enter 'username,password'");
-//
-//        StringTokenizer st = new StringTokenizer(result, ",");
-//        username = st.nextToken();
-//        password = st.nextToken();
+        String result = JOptionPane.showInputDialog("Enter 'username,password'");
 
-//        username = "vipjonpc@mail.ru";
-//        password = "webmailnokia35101989";
-        username = "jek2ka2016@yandex.ru";
-        password = "Nokia3510!";
+        StringTokenizer st = new StringTokenizer(result, ",");
+        username = st.nextToken();
+        password = st.nextToken();
 
         return new PasswordAuthentication(username, password);
     }
